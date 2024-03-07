a5302b550ac761a6a198a9b8ff295acfde213a0d
Fidan Bashirova <fidanbb@code.edu.az>
2024-03-07 23:21:35 +0400

    added cheking string ends with certain string

---
+++ StringEndCheck/StringEndCheck/Program.cs
@@ -0,0 +1,39 @@
+﻿//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//Examples:
+
+//solution('abc', 'bc') returns true
+//solution('abc', 'd') returns false
+
+#region solution-1
+//static bool Solution(string str, string ending)
+//{
+//    if (str.Length >= ending.Length)
+//    {
+//        return str.Substring(str.Length - ending.Length, ending.Length) == ending;
+
+//    }
+//    return false;
+//}
+
+//Console.WriteLine(Solution("abc", "bc"));
+//Console.WriteLine(Solution("abc", "d"));
+//Console.WriteLine(Solution("abc", "abcd"));
+
+#endregion
+
+#region solution-2
+//static bool Solution(string str, string ending)
+//{
+//    bool result = str.EndsWith(ending);
+//    return result;
+//}
+
+//Console.WriteLine(Solution("abc", "bc"));
+//Console.WriteLine(Solution("abc", "d"));
+//Console.WriteLine(Solution("abc", "abcd"));
+
+#endregion
+
+
+
